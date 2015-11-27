412366665c5f147ce58e85cddf30464ebdde9dd3
Lijun Xue <xljroy@gmail.com>
2015-11-26 21:49:20 -0800

    Migrate to github_trending_node

--- app/crawler.js
+++ app/crawler.js
@@ -1,9 +1,9 @@
-var Trending = require("github-trend");
-var scraper = new Trending.Scraper();
+var trend = require("github_trending_node");
+var crawler = new trend.Crawler();
  
 // Empty string means 'all languages'
 window.onload=function(){
-    scraper.scrapeTrendingRepos("").then(function(repos){
+    crawler.fetchTrendRepos("").then(function(repos){
         var txt = "<div id='repo-list'>";
         repos.forEach(function(repo){
             var link = "https://github.com/" + repo.owner + "/" + repo.name
