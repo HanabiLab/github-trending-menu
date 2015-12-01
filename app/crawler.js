98e4be8049d2a1e80a18156f1e3d02bd7b1362df
Lijun Xue <xljroy@gmail.com>
2015-11-30 19:27:14 -0800

    Modify interface
    some problem according to the crawler modules, need to be fix

--- app/crawler.js
+++ app/crawler.js
@@ -4,10 +4,10 @@ var crawler = new trend.Crawler();
 // Empty string means 'all languages'
 window.onload=function(){
     crawler.fetchTrendRepos("").then(function(repos){
-        var txt = "<div id='repo-list'>";
+        var txt = "<div id='repo-list' class='ui segments'>";
         repos.forEach(function(repo){
             var link = "https://github.com/" + repo.owner + "/" + repo.name
-            txt += "<a href=" + "'" + link + "'" + "><div class='repo-item ui red segment'>" + repo.owner + "/" + "<span style='font-weight: bold'>" +repo.name + "</span>" + "</div></a>" + "<br />";
+            txt += "<div class='repo-item ui raised segment'>" + repo.owner + "/" + "<a href=" + "'" + link + "'" + "><span style='font-weight: bold'>" +repo.name + "</span></a>" + "</div>";
         });
         txt += "</div>"
         document.getElementById('result').innerHTML = txt;
