f65c24ffdcebf87cc7d01bc7a430deea796ae1cf
Lijun(Roy) Xue <xljroy@gmail.com>
2016-03-09 00:50:14 -0800

    Change dataflow ui

--- app/crawler.js
+++ app/crawler.js
@@ -4,13 +4,16 @@ var crawler = new trend.Crawler();
 // Empty string means 'all languages'
 window.onload=function(){
     crawler.fetchTrendRepos("", function(repos){
-        var txt = "<div id='repo-list' class='ui segments'>";
+        var txt = "<div id='repo-list' class='ui relaxed divided list'>";
         repos.forEach(function(repo){
-            var link = repo.link
-            txt += "<div class='repo-item ui raised segment'>" + "<a href=" + "'" + link + "'" + "><span style='font-weight: bold'>" +repo.name + "</span></a>" + "</div>";
+            var body = "<div class='repo-item item'><i class='large github middle aligned icon'></i><div class='content'><a class='header' href=";
+            body +=  "'" + repo.link + "'";
+            body += ">" + repo.name + "</a><div class='description'>" + repo.desc +"</div>";
+            body += "</div></div>";
+            txt += body;
         });
-        txt += "</div>"
-        document.getElementById('result').innerHTML = txt;
+        txt += "</div>";
+        document.getElementById('all').innerHTML = txt;
     }).catch(function(err){
         console.log(err.message);
     });
